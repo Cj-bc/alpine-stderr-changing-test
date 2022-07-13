74a8be4998fc8828b8fabd01876e780b58ea596b
Cj-bc <cj.bc-sd@outlook.jp>
2022-07-13 11:33:53 +0900

    Make _ffi_test.py minimal as possible
    Removed unused code and comments

--- python-version/_ffi_test.py
+++ python-version/_ffi_test.py
@@ -4,11 +4,8 @@ ffibuilder = FFI()
 
 ffibuilder.set_source("_ffi_test", None)
 ffibuilder.cdef("""
-    /* from stdio.h */
     FILE* fopen(const char* path, const char* mode);
-    int fclose(FILE* fp);
-    extern FILE* stderr;  /* GNU C library */
-    extern FILE* __stderrp;  /* macOS */
+    extern FILE* stderr;
 """)
 
 if __name__ == '__main__':
